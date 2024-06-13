4d565f266fb591f4cb87f516a80e695eac6b907b
harisnaseer24 <harisnaseer258@gmail.com>
2024-06-13 13:30:09 +0500

    Wpf  crudlayout

---
+++ 2302C2WPFCRUD/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace _2302C2WPFCRUD
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ClearData()
+        {
+            pname.Clear();
+            desc.Clear();
+            price.Clear();
+            qty.Clear();
+            cat.Clear();
+        }
+
+        private void ClearFeilds(object sender, RoutedEventArgs e)
+        {
+            ClearData();
+        }
+    }
+}
