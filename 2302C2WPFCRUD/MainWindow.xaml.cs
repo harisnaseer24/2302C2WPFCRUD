d76c13a97b5e75d5dcd9b1d0ef6d1359cb6f8e9a
harisnaseer24 <harisnaseer258@gmail.com>
2024-06-15 14:14:05 +0500

    db connection and insert operation done

--- 2302C2WPFCRUD/MainWindow.xaml.cs
+++ 2302C2WPFCRUD/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Data;
+using System.Windows;
+using Microsoft.Data.SqlClient;
 
 namespace _2302C2WPFCRUD
 {
@@ -9,6 +11,12 @@ namespace _2302C2WPFCRUD
         {
             InitializeComponent();
         }
+
+
+        SqlConnection Conn = new SqlConnection("Data Source=.;Initial Catalog=2302C2WPFCRUD;User ID=sa;Password=aptech;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+
+
+       
 
         private void ClearData()
         {
@@ -22,6 +30,44 @@ namespace _2302C2WPFCRUD
         private void ClearFeilds(object sender, RoutedEventArgs e)
         {
             ClearData();
+        }
+
+        private bool isValid()
+        {
+            if (pname.Text == string.Empty || desc.Text == string.Empty || price.Text == string.Empty || qty.Text == string.Empty || cat.Text == string.Empty) {
+
+
+                MessageBox.Show("All fields are required", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+
+        private void AddProduct(object sender, RoutedEventArgs e)
+        {
+            if (isValid())
+            {
+                SqlCommand addprod = new SqlCommand("Insert into products values (@pname, @desc, @price, @qty,@cat);", Conn);
+
+                addprod.CommandType = CommandType.Text;
+                Conn.Open();
+                addprod.Parameters.AddWithValue("@pname", pname.Text);
+                addprod.Parameters.AddWithValue("@desc", desc.Text);
+                addprod.Parameters.AddWithValue("@price", price.Text);
+                addprod.Parameters.AddWithValue("@qty", qty.Text);
+                addprod.Parameters.AddWithValue("@cat", cat.Text);
+                addprod.ExecuteNonQuery();
+
+                Conn.Close();
+                MessageBox.Show("Product inserted successfully", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                ClearData();
+
+            }
         }
     }
 }
